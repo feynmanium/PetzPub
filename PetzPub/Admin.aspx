fe4189b50327d0bf5c215b3a3698597d915b073f
iillll <jcjahrau@neiu.edu>
2016-12-05 15:29:30 -0600

    Admin Page Functionality
    Currently returns error

---
+++ PetzPub/Admin.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="PetzPub.Admin" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <asp:Label ID="Label1" class="control-label" runat="server" Text="Add a product:"></asp:Label>
+        <br />
+        <asp:Label ID="Label2" class="control-label" runat="server" Text="Product Name: "></asp:Label>
+        <asp:TextBox ID="TextBoxProductName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" class="control-label" runat="server" Text="Price: $"></asp:Label>
+        <asp:TextBox ID="TextBoxPrice" runat="server" TextMode="Number"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Product Description: "></asp:Label>
+        <asp:TextBox ID="TextBoxDescription" runat="server" TextMode="MultiLine"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Animal: "></asp:Label>
+        <asp:DropDownList ID="DropDownListAnimal" runat="server">
+            <asp:ListItem>Dog</asp:ListItem>
+            <asp:ListItem>Cat</asp:ListItem>
+            <asp:ListItem>Bird</asp:ListItem>
+            <asp:ListItem>Small Animal</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Category: "></asp:Label>
+        <asp:DropDownList ID="DropDownListCategory" runat="server">
+            <asp:ListItem>Food</asp:ListItem>
+            <asp:ListItem>Accessory</asp:ListItem>
+            <asp:ListItem>Toy</asp:ListItem>
+            <asp:ListItem>Treat</asp:ListItem>
+            <asp:ListItem>Apparel</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label7" runat="server" Text="Image URL: "></asp:Label>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Label ID="LabelOutput" runat="server" Text=""></asp:Label>
+        <asp:Image ID="ImageOutput" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Add Product" OnClick="Button1_Click" />
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Products]"></asp:SqlDataSource>
+</asp:Content>
---
+++ PetzPub/Admin.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PetzPub
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string productName = TextBoxProductName.Text;
+            string productPrice = TextBoxPrice.Text;
+            string productDescription = TextBoxDescription.Text;
+            string productAnimal = DropDownListAnimal.Text;
+            string productCategory = DropDownListCategory.Text;
+            string productImageUrl = "";
+
+            if (Page.IsPostBack)
+            {
+                Boolean fileOK = false;
+                string path = Server.MapPath("~/Images/");
+
+                if (FileUpload1.HasFile)
+                {
+                    String fileExtension = System.IO.Path.GetExtension(FileUpload1.FileName).ToLower();
+                    String[] allowedExtensions = { ".gif", ".png", ".jpeg", ".jpg" };
+                    for (int i = 0; i < allowedExtensions.Length; i++)
+                    {
+                        if (fileExtension == allowedExtensions[i])
+                        {
+                            fileOK = true;
+                        }
+                    }
+                }
+
+                if (fileOK)
+                {
+                    try
+                    {
+                        FileUpload1.PostedFile.SaveAs(path + FileUpload1.FileName);
+                        LabelOutput.Text = "File uploaded to " + "~/Images/" + FileUpload1.FileName;
+                        ImageOutput.ImageUrl = "~/Images/" + FileUpload1.FileName;
+                        productImageUrl = "~/Images/" + FileUpload1.FileName;
+
+                        SqlConnection connection = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename =| DataDirectory |\\Database1.mdf; Integrated Security = True");
+                        SqlCommand cmd = new SqlCommand("INSERT INTO Products(product_name, price, description, animal, category, imageUrl) VALUES ('" + productName + "', " + productPrice + ", '" + productDescription + "', '" + productAnimal + "', '" + productCategory + "', '" + productImageUrl + "')");
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        LabelOutput.Text = ex.Message;
+                    }
+                }
+                else
+                {
+                    LabelOutput.Text = "This file could not be uploaded";
+                }
+
+            }
+        }
+    }
+}
