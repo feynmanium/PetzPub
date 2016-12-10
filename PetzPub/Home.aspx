72cddab490ac2ed12c7c8cba60d09ef76b9b3641
iillll <jcjahrau@neiu.edu>
2016-12-10 12:08:58 -0600

    Added Home.aspx code and our own stylesheet.
    The Homepage now displays the first 9 products in the database. I'm not
    at all tied to this idea or implementation, it's just the only thing I
    could think of

--- PetzPub/Home.aspx
+++ PetzPub/Home.aspx
@@ -1,6 +1,32 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="PetzPub.Home" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-    <link href="Content/bootstrap.css" rel="stylesheet" />
+    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2><u>Featured Products</u></h2>
+    <div class="row">
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton1" height="400px" onClick="onImageClick" runat="server" /></div>
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton2" height="400px" onClick="onImageClick" runat="server" /></div>
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton3" height="400px" onClick="onImageClick" runat="server" /></div>
+    </div>
+        <div class="row">
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton4" height="400px" onClick="onImageClick" runat="server" /></div>
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton5" height="400px" onClick="onImageClick" runat ="server" /></div>
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton6" height="400px" onClick="onImageClick" runat="server" /></div>
+    </div>   
+    <div class="row">
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton7" height="400px" onClick="onImageClick" runat="server" /></div>
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton8" height="400px" onClick="onImageClick" runat="server" /></div>
+        <div class="col-md-4">
+            <asp:ImageButton ID="ImageButton9" height="400px" onClick="onImageClick" runat="server" /></div>
+    </div>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT [imageUrl], [Id] FROM [Products]"></asp:SqlDataSource>
 </asp:Content>
--- PetzPub/Home.aspx.cs
+++ PetzPub/Home.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,88 @@ namespace PetzPub
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+            SqlCommand cmd;
+            string command = "SELECT [Id], [imageUrl] FROM[Products]";
+            
+            cmd = new SqlCommand(command);
+            cmd.Connection = connection;
 
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    string url = reader["imageUrl"].ToString();
+                    if (url != null)
+                        ImageButton1.ImageUrl = url;
+                }
+
+                if (reader.Read())
+                {
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton2.ImageUrl = url;
+                }
+                if (reader.Read())
+                {
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton3.ImageUrl = url;
+                }
+                if (reader.Read())
+                {
+
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton4.ImageUrl = url;
+                }
+                if (reader.Read())
+                {
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton5.ImageUrl = url;
+                }
+
+                if (reader.Read())
+                {
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton6.ImageUrl = url;
+                }
+                if (reader.Read())
+                {
+
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton7.ImageUrl = url;
+                }
+                if (reader.Read())
+                {
+
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton8.ImageUrl = url;
+                }
+                if (reader.Read())
+                {
+
+                    string url = reader["imageURL"].ToString();
+                    if (url != null)
+                        ImageButton9.ImageUrl = url;
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+        }
+
+        protected void onImageClick(object sender, EventArgs e)
+        {
+            Response.Redirect("Product.aspx");
         }
     }
 }
